357bf0fbd1ed2c93df29a15fefe72b2363050bd6
Sushmitha Rai <sushmitharai210@gmail.com>
2024-05-23 22:20:02 +0530

    Update MajorityElement.py

--- Leetcode150/MajorityElement.py
+++ Leetcode150/MajorityElement.py
@@ -1,4 +1,5 @@
 # This is solved using Moore's Voting Algorithm, refer random-problems/notes/MooreVotingAlgo
+# Leetcode: https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = 0
